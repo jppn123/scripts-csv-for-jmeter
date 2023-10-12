1d5b3212d1e162cda1f843bbf55353fc576bc67b
joao pedro barros <jppn123>
2023-10-12 16:35:54 -0300

    modificando a resposta adicionando estado ao crm

--- medic_create_csv.py
+++ medic_create_csv.py
@@ -1,21 +1,24 @@
 import pandas as pd
 from br_nome_gen import pessoa_random
-from random import randrange
+from random import randrange, choice
 
 crm = []
 names = []
+estados= ["AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MS", "MT", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"]
 nCol = 10
 for x in range(nCol):
     names.append(pessoa_random().nome)
 
 for x in range(nCol):
-    randCrm = randrange(start=100000000000000, stop=1000000000000000000000000)
+    randCrm = randrange(start=100, stop=100000000000000)
     if randCrm not in crm:
-        crm.append(randCrm)
+        crm.append(str(randCrm) + "/" + choice(estados))
     else:
-        randCrm = randrange(start=110003000400000000, stop=100000000000000000000)
-        crm.append(randCrm)
-        
+        randCrm = randrange(start=30000, stop=5000000)
+        crm.append(str(randCrm) + "/" + choice(estados))
+
+
+
 df = pd.DataFrame(
     {
     'crm': crm,
