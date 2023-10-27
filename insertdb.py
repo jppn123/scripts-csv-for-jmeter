95321addb7f6425f1bd3f9bbb5adb132301dc5a4
jppn123 <jpiluca11@gmail.com>
2023-10-27 14:09:58 -0300

    .

--- insertdb.py
+++ insertdb.py
@@ -7,13 +7,14 @@ file = open("statistics.csv")
 csvreader = csv.reader(file)
 values = [val for val in csvreader]
 data = {
-    'sampler': [x[1] for x in values[:-1]], 
-    'average': [x[2] for x in values[:-1]],
-    'median': [x[3] for x in values[:-1]],
-    '90_line': [x[4] for x in values[:-1]],
-    '95_line': [x[5] for x in values[:-1]],
-    '99_line': [x[6] for x in values[:-1]],
-    'error_percent': [x[9] for x in values[:-1]]
+    'sampler': [int(x[1]) for x in values[:-1]], 
+    'average': [float(x[2]) for x in values[:-1]],
+    'median': [float(x[3]) for x in values[:-1]],
+    '90_line': [float(x[4]) for x in values[:-1]],
+    '95_line': [float(x[5]) for x in values[:-1]],
+    '99_line': [float(x[6]) for x in values[:-1]],
+    'error_percent': [float(x[9][:-1]) for x in values[:-1]],
+    'throughput': [float(x[10]) for x in values[:-1]]
 }
 
 db_url = 'postgresql://postgres:admin@localhost:5432/jmeter-csv-stats'
